1d1d1e262faba0f346cc8203d767757658d62848
mmsstudent2021 <mmsstudent2021@gmail.com>
2023-11-07 11:02:46 +0630

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./index.css";
 import { categoryBtn, categoryList } from "./src/core/selectors";
-import Shop from "./src/shop";
+import Shop from "src/Shop.js";
 
 const shop = new Shop();
 shop.init();
